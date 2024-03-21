998a9e23f286a37d31fd622f0574a7523304baec
Cosmin <cosmin.catalin270@gmail.com>
2024-03-21 09:29:36 +0200

    Optimal parameters to be changed

--- xgboost-implementation.py
+++ xgboost-implementation.py
@@ -16,43 +16,55 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, strat
 print(sum(y_train) / len(y_train))
 print(sum(y_test) / len(y_test))
 #round 1
-#param_grid = {
+# param_grid = {
 #    'max_depth': [3, 4, 5],
 #    'learning_rate': [0.1, 0.01, 0.05],
 #    'gamma': [0, 0.25, 1.0],
 #    'reg_lambda': [0.1, 1.0, 10.0],
 #    'scale_pos_weight': [1, 3, 5]
-#}
+# }
 
 
 #round 2
-param_grid = {
-    'max_depth': [4],
-    'learning_rate': [0.05],
-    'gamma': [1.0],
-    'reg_lambda': [1.0],
-    'scale_pos_weight': [5]
-}
-#colsample_bytree parameter for overfitting
-#subsample parameter for overfitting
-#optimal_params = GridSearchCV(
+# param_grid = {
+#     'max_depth': [4],
+#     'learning_rate': [0.05],
+#     'gamma': [1.0],
+#     'reg_lambda': [1.0],
+#     'scale_pos_weight': [5]
+# }
+
+# optimal_params = GridSearchCV(
 #    estimator=XGBClassifier(objective='binary:logistic', seed=42, eval_metric='aucpr',use_label_encoder=False, early_stopping_rounds=10),
 #    param_grid=param_grid,
 #    scoring='roc_auc',
 #    verbose=0,
 #    cv=3
-#)
+# )
 
 
-#optimal_params.fit(X_train,
+# optimal_params.fit(X_train,
 #            y_train,
 #            eval_set=[(X_test, y_test)])
 
-#print(optimal_params)
+# print(optimal_params.best_params_)
 
+#colsample_bytree parameter for overfitting
+#subsample parameter for overfitting
+clf_xgb = XGBClassifier(objective='binary:logistic',
+                        # max_depth=4,
+                        # learning_rate=0.05,
+                        # gamma=1.0,
+                        # reg_lambda=1.0,
+                        # scale_pos_weight=5,
+                        seed=42,
+                        eval_metric='aucpr',
+                        use_label_encoder=False,
+                        early_stopping_rounds=10,)
 
-clf_xgb = XGBClassifier(objective='binary:logistic', seed=42, eval_metric='aucpr',use_label_encoder=False, early_stopping_rounds=10)
-
+clf_xgb.fit(X_train,
+            y_train,
+            eval_set=[(X_test, y_test)])
 
 ConfusionMatrixDisplay.from_estimator(clf_xgb, X_test, y_test, display_labels=['Loss', 'Win'])
 plt.show()
